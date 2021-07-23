8a553f5cf115fa99d819e7fe42dcf256af90a86f
David Mwendwa <klatzdavie19@gmail.com>
2021-07-23 09:42:33 +0300

    initial commit - enable Google OAuth API

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
