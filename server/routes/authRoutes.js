00a5244f6d6a311d22bc8b3dc4b445ce07cc43a9
David Mwendwa <klatzdavie19@gmail.com>
2021-07-25 18:59:31 +0300

    refactor: server side structure

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+
+// Whenever a user gets into this route, we kick them back to the OAuth flow
+module.exports = (app) => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+}
