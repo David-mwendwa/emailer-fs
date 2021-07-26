3ff3acf4434495a512a7cfd55e2a1ce27455b44d
David Mwendwa <klatzdavie19@gmail.com>
2021-07-26 11:39:08 +0300

    test: test current_user route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -16,11 +18,20 @@ try {
 }
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
-
-//mongodb+srv://david-dev:<password>@cluster0.6rzt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
