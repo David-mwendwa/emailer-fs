32154c924153497cf2b8dc2aeba850d9b0efd964
David Mwendwa <klatzdavie19@gmail.com>
2021-07-26 10:05:40 +0300

    feat: implement saving of model instance in the db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
-require('./models/User')
 
 const mongoAtlasUri = keys.mongoURI;
 try {
