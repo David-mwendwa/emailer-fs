c0d13a1ab69981e4597ebf3c321b770e8b28d578
David Mwendwa <klatzdavie19@gmail.com>
2021-07-26 09:25:42 +0300

    feat: connect mongoose to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 require('./services/passport');
+
+const mongoAtlasUri = keys.mongoURI;
+try {
+  mongoose.connect(
+    mongoAtlasUri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to mongoDB')
+  );
+} catch (e) {
+  console.log('could not connect', e);
+}
 
 const app = express();
 require('./routes/authRoutes')(app);
@@ -8,3 +21,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+
+//mongodb+srv://david-dev:<password>@cluster0.6rzt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
