6d805d7dcd144dcd86f475d8402c6a1b128197e1
David Mwendwa <klatzdavie19@gmail.com>
2021-07-29 08:23:19 +0300

    feat: implement version control scheme

---
+++ server/config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+//819284387565-d2j07vj2sf8ile30fhirsu6m4vmh7rna.apps.googleusercontent.com
+//nWYR1akw7oeBdUyMz3fk9lVt
