09ccfe2bf0c0c597d9d8ffc721c89c0c94e90662
David Mwendwa <klatzdavie19@gmail.com>
2021-08-18 17:56:08 +0300

    fix: work on proxy stumbling block

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,9 +1,11 @@
+//import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Emailer
+    <div className='App'>
+      <h2>Emailer</h2>
+      <a href='/auth/google'>Sign In With Google</a>
     </div>
   );
 }
