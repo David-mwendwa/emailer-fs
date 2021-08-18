d8acd9e765bb28629b8487fa84523ce49cfc583e
David Mwendwa <klatzdavie19@gmail.com>
2021-08-18 19:46:35 +0300

    feat: redux store setup

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 ReactDOM.render(
-  <Router>
+  <Provider store={store}>
     <App />
-  </Router>,
+  </Provider>,
   document.getElementById('root')
 );
-
