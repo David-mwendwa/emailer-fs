10cce37c00ee2fe0e25faee423cdbaf0ab211404
David Mwendwa <klatzdavie19@gmail.com>
2021-08-19 06:17:27 +0300

    feat: add route configuration

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,19 @@
 //import { Link } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => {
   return (
-    <div className='App'>
-      <h2>Emailer</h2>
-      <a href='/auth/google'>Sign In With Google</a>
+    <div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Landing} />
+        </div>
+      </Router>
     </div>
   );
 }
