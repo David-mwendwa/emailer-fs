933a9bb9aee834ae68d3aae30739a01e347a7ee3
David Mwendwa <klatzdavie19@gmail.com>
2021-08-20 10:10:34 +0300

    feat: display credit quantity to the client

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -10,11 +10,18 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return <li><a href="/auth/google">Login with Google</a></li>
+        return (
+          <li>
+            <a href='/auth/google'>Login with Google</a>
+          </li>
+        );
       default:
         return (
           <Fragment>
-            <li key='1'><Payments /></li>
+            <li key='1'>
+              <Payments />
+            </li>
+            <li key='3' style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>
             <li key='2'>
               <a href='/api/logout'>Logout</a>
             </li>
@@ -26,7 +33,9 @@ class Header extends Component {
     return (
       <nav>
         <div className='nav-wrapper'>
-          <Link to={this.props.auth ? '/surveys' : '/'} className='left brand-logo'>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className='left brand-logo'>
             Emailer
           </Link>
           <ul id='nav-mobile' className='right'>
