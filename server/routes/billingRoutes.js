939e5acc5bb8755f6df2dbf2bb59596af1692ab1
David Mwendwa <klatzdavie19@gmail.com>
2021-08-20 09:50:55 +0300

    feat: add authentication to the _biling route_

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,8 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = (app) => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) return res.status(401).send({ error: 'You must log in!' });
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
@@ -10,8 +12,8 @@ module.exports = (app) => {
       source: req.body.id,
     });
 
-    req.user.credits += 5
+    req.user.credits += 5;
     const user = await req.user.save();
-    res.send(user)
+    res.send(user);
   });
 };
