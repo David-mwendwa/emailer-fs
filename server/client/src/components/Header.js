d950e084c17490b6d7994c7e4763fde8df30b177
David Mwendwa <klatzdavie19@gmail.com>
2021-08-20 07:29:18 +0300

    feat: customize the StripeCheckout

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -14,8 +14,8 @@ class Header extends Component {
       default:
         return (
           <Fragment>
-            <li><Payments /></li>
-            <li>
+            <li key='1'><Payments /></li>
+            <li key='2'>
               <a href='/api/logout'>Logout</a>
             </li>
           </Fragment>
