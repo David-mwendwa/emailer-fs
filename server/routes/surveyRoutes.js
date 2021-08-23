dfa59ae7335899930e102e5c8ba13bc8d738d297
David Mwendwa <klatzdavie19@gmail.com>
2021-08-23 08:33:05 +0300

    feat: create surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,17 @@
-const requireLogin = require('../middlewares/requireLogin')
-const requireCredits = require('../middlewares/requireCredits')
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 
-module.exports = app => {
+const Survey = mongoose.model('surveys');
+
+module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
-  })
-}
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+    });
+  });
+};
